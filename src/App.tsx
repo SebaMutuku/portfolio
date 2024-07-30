e75934b91fad6ac3632cfb3d130ad9c93c80d5cc
SebaMutuku <sebastian.muthiani@gmail.com>
2024-07-30 10:31:48 +0300

    Use browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
 import React from 'react';
 import Home from "src/screens/home";
-import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 import Education from "src/screens/education";
 import Experience from "src/screens/experience";
 import ContactMe from "src/screens/contactme";
 
 
-const Navbar = () => {
-    return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/education">Education</Link>
-            <Link to="/experience">Experience</Link>
-            <Link to="/contactme">Contact Me</Link>
-        </div>
-    )
-}
-
-
 function App() {
     return (
-        <Router basename="/">
-            <Home/>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/education" element={<Education/>}/>
-                <Route path="/experience" element={<Experience/>}/>
-                <Route path="/contactme" element={<ContactMe/>}/>
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/education" element={<Education/>}/>
+            <Route path="/experience" element={<Experience/>}/>
+            <Route path="/contactme" element={<ContactMe/>}/>
+        </Routes>
 
     )
         ;
