c979d3c118b031faa0b90817269242011d000743
SebaMuutku <sebastian.muthiani@gmail.com>
2025-09-03 15:32:46 +0300

    Revert app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import Home from 'src/screens/home';
-import Education from 'src/screens/education';
-import Experience from 'src/screens/experience';
-import ContactMe from 'src/screens/contactme';
-import Projects from 'src/screens/projects';
-import {Route, Routes} from 'react-router-dom';
+import Home from "src/screens/home";
+import {Route, Routes} from 'react-router-dom'
+import Education from "src/screens/education";
+import Experience from "src/screens/experience";
+import ContactMe from "src/screens/contactme";
+import Projects from "src/screens/projects";
 
-function App(): JSX.Element {
+
+function App() {
     return (<Routes>
             <Route index path="/" element={<Home/>}/>
             <Route path="/education" element={<Education/>}/>
